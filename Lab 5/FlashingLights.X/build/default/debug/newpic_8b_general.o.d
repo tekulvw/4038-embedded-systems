f30797f822f40bb94a7ebb3feecf12f11e4fb7f2
Will Tekulve <tekulve.will@gmail.com>
2019-10-10 17:38:14 -0400

    add flashing lights project

---
+++ Lab 5/FlashingLights.X/build/default/debug/newpic_8b_general.o.d
@@ -0,0 +1,3 @@
+build/default/debug/newpic_8b_general.o:  \
+	/home/will/Documents/UC/Embedded\ Systems/Git/Lab\ 5/Attempt3.X/newpic_8b_general.asm  \
+
---
+++ Lab 5/FlashingLights.X/build/default/production/newpic_8b_general.o.d
@@ -0,0 +1,3 @@
+build/default/production/newpic_8b_general.o:  \
+	/home/will/Documents/UC/Embedded\ Systems/Git/Lab\ 5/Attempt3.X/newpic_8b_general.asm  \
+
